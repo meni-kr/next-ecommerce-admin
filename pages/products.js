432784c33e20884656f53f2ec3ab7a12e7c81a1a
meni-kr <meni.kramer@gmail.com>
2024-09-24 12:05:21 +0300

    Get products from mongodb to products page and show them

--- pages/products.js
+++ pages/products.js
@@ -1,12 +1,45 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
-export default function Products(){
+export default function Products() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/products')
+            .then(response => {
+                setProducts(response.data)
+            })
+    }, [])
+
+    console.log('products:', products)
 
     return (
         <Layout>
-            <Link className={'bg-blue-900 text-white rounded-md py-1 px-2'} href={'/products/new'}>Add new product</Link>
+            <Link className={'bg-blue-900 text-white rounded-md py-1 px-2'} href={'/products/new'}>
+                Add new product</Link>
+            <table className="basic mt-2">
+                <thead>
+                    <tr>
+                        <td>Product name</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, idx) => (
+                            <tr key={idx}>
+                                <td>{product.title}</td>
+                                <td>
+                                    
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </Layout>
     )
 }
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,18 +1,42 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-export default function NewProduct(){
+export default function NewProduct() {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [price, setPrice] = useState('')
+    const [goToProducts, setGoToProducts] = useState(false)
 
+    const router = useRouter()
 
-    return(
+    async function createProduct(e) {
+        e.preventDefault()
+        const data = { title, description, price }
+        await axios.post('/api/products', data)
+        setGoToProducts(true)
+    }
+    if (goToProducts) router.push('/products')
+    return (
         <Layout>
-            <h1>New Product</h1>
-            <label>Product name:</label>
-            <input type="text" placeholder="product name"/>
-            <label>Description:</label>
-            <textarea placeholder="description"></textarea>
-            <label>Price:</label>
-            <input type="number" placeholder="price"/>
-            <button className="btn-primary">Save</button>
+            <form onSubmit={createProduct}>
+                <h1>New Product</h1>
+                <label>Product name:</label>
+                <input type="text" placeholder="product name"
+                    value={title} onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Description:</label>
+                <textarea placeholder="description"
+                    value={description} onChange={(e) => setDescription(e.target.value)}
+                />
+                <label>Price:</label>
+                <input type="number" placeholder="price"
+                    value={price} onChange={(e) => setPrice(e.target.value)}
+                />
+                <button type="submit" className="btn-primary">Save</button>
+            </form>
+
         </Layout>
     )
 }
