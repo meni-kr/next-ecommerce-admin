5a549ebdf6b8eb1fb0d06183fa3413ace226305d
meni-kr <meni.kramer@gmail.com>
2024-10-07 18:23:51 +0300

    Show orders in order page

--- pages/orders.js
+++ pages/orders.js
@@ -1,10 +1,49 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function Orders(){
 
-    return(
+export default function OrdersPage() {
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/orders')
+            .then(response => {
+                setOrders(response.data)
+            })
+    }, [])
+
+    return (
         <Layout>
-            Orders page
+            <h1>Orders</h1>
+            <table className="basic">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Recipient</th>
+                        <th>Products</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.length > 0 && orders.map((order, idx) => (
+                        <tr key={idx}>
+                            <td>{new Date(order.createdAt).toLocaleString()}</td>
+                            <td>
+                                {order.name} {order.email}<br />
+                                {order.city} {order.postalCode} {order.country}<br />
+                                {order.streetAddress}
+                            </td>
+                            <td>
+                                {order.line_items.map((line, idx) => (
+                                    <span key={idx}>
+                                        {line.price_data.product_data.name} x {line.quantity}<br />
+                                    </span>
+                                ))}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Layout>
     )
 }
